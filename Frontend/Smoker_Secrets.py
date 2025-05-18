112822697e358edf4b7ccdecb1ce29a604d20a48
costasvav <vavourakis@gmail.com>
2025-05-18 11:38:20 -0500

    Reorg files

---
+++ Frontend/Smoker_Secrets.py
@@ -0,0 +1,9 @@
+# InfluxDB Configuration
+INFLUX_HOST = "localhost"
+INFLUX_PORT = 8086
+INFLUX_URL = f"http://{INFLUX_HOST}:{INFLUX_PORT}"
+INFLUX_ORG = "opensmoker"
+INFLUX_BUCKET = "opensmoker_mqtt_data"
+INFLUX_TOKEN="DAvOyR2iJzwWNSPNrNHqntO4c0HIB26M1hotqgTuYuQLEbIAGAyU7Gszol086GcDO2E5kURgnpE67n5cFv1Lmw=="
+
+
